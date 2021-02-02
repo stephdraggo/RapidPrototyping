6ad79b486129fe41cb6604271c3081f150411025
stephdraggo <stephdraggo@users.noreply.github.com>
2021-02-02 12:27:03 +1100

    yee

---
+++ Assets/Scripts/AsteroidMaze/MazeGeneration.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AsteroidMaze
+{
+    public class MazeGeneration : MonoBehaviour
+    {
+        #region Variables
+        float mazeWidth, mazeHeight;
+        #endregion
+
+        #region Start
+        void Start()
+        {
+            
+        }
+        #endregion
+
+        #region Update
+        void Update()
+        {
+
+        }
+        #endregion
+
+        #region Methods
+
+        #endregion
+    }
+}
